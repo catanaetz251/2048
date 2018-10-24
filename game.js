42b6ec6bdd86c24af8fa06d2f28aca7b1987a16b
catanaetz251 <insert github email here>
2018-10-24 21:39:45 +0300

    first commit

---
+++ game.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function()  {
+
+    $('.grid-container').children('.grid-row').each((index, element) => {
+        console.log (element);
+        console.log (index);
+    });
+
+});
+

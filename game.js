9c46ba5e5ab771cfbc3459aa0df34c8218ce873e
catanaetz251 <you@example.com>
2018-11-04 10:54:56 +0200

    ref

--- game.js
+++ game.js
@@ -4,20 +4,14 @@ $(document).ready(function () {
         putRandomTile();
     });
 });
-
-// functia de adaugare random tile in grid
+// functia de adaugare random tile in gridul de 4x4
 function putRandomTile() {
-
     // caut un random cell in care sa pun unul din cele 2 cell-uri
     let randomCell = Math.floor(Math.random() * $('.grid-cell').length);
-
-    if ($(randomCell).hasClass('grid-cell-two')) {
-        putRandomTile();
-    } else if ($(randomCell).hasClass('grid-cell-four')) {
-        putRandomTile();
+    if ($(randomCell).hasClass('grid-cell-two') || $(randomCell).hasClass('grid-cell-four')) {
+        putRandomTile();    
     } else {
         $('.grid-cell').eq(randomCell).removeClass('grid-cell').addClass(function () {
-
             // generez un numar random de la 0 la 100 si trimit ca parametru clasa two sau four in functie de procent
             let randomNumber = Math.floor(Math.random() * 100);
             if (randomNumber <= 90) {
